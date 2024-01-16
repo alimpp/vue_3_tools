ecec8c4df200b062dec3e8f41780ce3e62308774
alimpp <hmdmpp@gmail.com>
2024-01-16 04:07:18 -0800

    Add core select basic

---
+++ src/components/core/CoreSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>{{ label }}</span>
+    <select v-model="selectValue" @change="seletItem">
+      <option v-for="item in dataSource" :key="item.id">
+        {{ item.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits, defineProps } from "vue";
+
+const selectValue = ref("");
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  dataSource: {
+    type: Array,
+    default: [],
+  },
+});
+
+const emit = defineEmits(["emitedSelectedValue"]);
+
+const seletItem = () => {
+  emit("emitedSelectedValue", selectValue.value);
+};
+</script>
